e4fc3d5c2916d17aaadad26a9bb20d7a51c743fe
Janna May <a.jannamay@wustl.edu>
2024-11-07 17:16:58 -0600

    upload files from Anang

---
+++ organelle_measure/tools.py
@@ -0,0 +1,189 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from scipy import ndimage as ndi
+from skimage import io,morphology,measure,segmentation
+
+# BEGIN of ND2reader wrapper:
+from nd2reader import ND2Reader
+
+def get_nd2_size(path:str):
+    with ND2Reader(path) as images:
+        size = images.sizes
+    return size
+
+def load_nd2_plane(path:str,frame:str='cyx',axes:str='tz',idx:int=0):
+    """read an image flexibly with ND2Reader."""
+    with ND2Reader(path) as images:
+        images.bundle_axes = frame
+        images.iter_axes = axes
+        img = images[idx]
+    return img.squeeze()
+# END of ND2reader wrapper.
+
+# START of organelle nd2 file opener
+def open_golgi(path):
+    if 'c' in get_nd2_size(path):
+        return np.mean(load_nd2_plane(str(path),frame="czyx",axes='t',idx=0).astype(int),axis=0,dtype=int)
+    else:
+        return load_nd2_plane(str(path),frame="zyx",axes='t',idx=0).astype(int)
+def open_mito(path):
+    if Path(path).suffix == ".nd2":
+        return load_nd2_plane(str(path),frame="zyx",axes='tc',idx=0).astype(int)
+    else:
+        img = io.imread(str(path))
+        img_raw = np.zeros([int(img.shape[0]/2),*img.shape[1:]],dtype=int)
+        for z in range(img_raw.shape[0]):
+            img_raw[z] = img[2*z]
+        return img_raw
+def open_LD(path):
+    if Path(path).suffix == ".nd2":
+        return load_nd2_plane(str(path),frame="zyx",axes='tc',idx=1).astype(int)
+    else:
+        img = io.imread(str(path))
+        img_raw = np.zeros([int(img.shape[0]/2),*img.shape[1:]],dtype=int)
+        for z in range(img_raw.shape[0]):
+            img_raw[z] = img[2*z+1]
+        return img_raw
+open_organelles = {
+    "peroxisome":   lambda x: load_nd2_plane(str(x),frame="zyx",axes='tc',idx=0).astype(int),
+    "vacuole":      lambda x: load_nd2_plane(str(x),frame="zyx",axes='tc',idx=1).astype(int),
+    "ER":           lambda x: load_nd2_plane(str(x),frame="zyx",axes='t', idx=0).astype(int),
+    "golgi":        open_golgi,
+    "mitochondria": open_mito,
+    "LD":           open_LD
+}
+# END of organelle nd2 file opener
+
+# START of vacuole postprocesser
+
+def skeletonize_zbyz(binary3d):
+    """image has values [0,1] only."""
+    skeletonized = np.zeros_like(binary3d)
+    for z in range(len(skeletonized)):
+        skeletonized[z] = morphology.skeletonize(binary3d[z])
+    return skeletonized
+def watershed_zbyz(skeleton3d):
+    reversed3d = ~skeleton3d
+    img_dist = np.zeros_like(reversed3d,dtype=float)
+    img_wtsd = np.zeros_like(reversed3d,dtype=int)
+    for z in range(len(reversed3d)):
+        img_dist[z] = ndi.distance_transform_edt(reversed3d[z])
+        img_wtsd[z] = segmentation.watershed(-img_dist[z],mask=reversed3d[z],)
+    return img_wtsd
+
+
+##useful for pthon less than 3.11
+# def find_complete_rings(skeleton3d):
+#     core3d = np.zeros_like(skeleton3d,dtype=int)
+#     for z in range(core3d.shape[0]):
+#         skeleton3d[z] = segmentation.clear_border(skeleton3d[z])
+#         core3d[z] = measure.label(morphology.flood_fill(~skeleton3d[z],(0,0),False,selem=morphology.disk(1)))
+#     return core3d
+
+
+
+##For python 3.11
+def find_complete_rings(skeleton3d):
+    core3d = np.zeros_like(skeleton3d, dtype=int)
+    for z in range(core3d.shape[0]):
+        skeleton3d[z] = segmentation.clear_border(skeleton3d[z])
+        filled_image = morphology.flood_fill(~skeleton3d[z], (0, 0), False, connectivity=1)
+        labeled_image = measure.label(filled_image)
+        core3d[z] = labeled_image
+    return core3d
+
+
+def intersection_over_union(bool1,bool2):
+    return np.count_nonzero(bool1*bool2)/np.count_nonzero(bool1+bool2)
+def find_hidden_object(expand2d,watershed2d,coord):
+    
+    
+    morphology.f
+    """
+    expand2d: 2d integer image with the objects on previous plane.
+    watershed: 2d watershed image on the current plane
+    label: label of the object of interest
+    """
+    mask_last = (expand2d==expand2d[coord])
+    label = watershed2d[coord]
+    mask_this = (watershed2d==label)
+    iou = intersection_over_union(mask_this,mask_last)
+    if iou<0.5 or (np.count_nonzero(mask_this)>1.5*np.count_nonzero(mask_last)):
+        return None
+    else:
+        return label
+def better_vacuole_img(filled3d,watershed3d):
+    core3d = measure.label(filled3d)
+    num_z = core3d.shape[0]
+    expanded3d = np.copy(core3d)
+    for prop in measure.regionprops(core3d):
+        # exclude very small blobs
+        if prop.area<20:
+            expanded3d[core3d==prop.label] = 0
+            continue
+        # find the range on z axis
+        z_coords = prop.coords[:,0]
+        z_max,z_min = z_coords.max(),z_coords.min()
+        centroid = tuple(int(coo) for coo in prop.centroid[1:])
+        
+        # expand towards -z direction
+        ref_xpnd = expanded3d[z_min]
+        for z in range(z_min-1,-1,-1):
+            ref_wtsd = watershed3d[z]
+            chosen = find_hidden_object(ref_xpnd,ref_wtsd,centroid)
+            if chosen is None:
+                break
+            expanded3d[z,ref_wtsd==chosen] = prop.label
+            ref_xpnd = expanded3d[z]
+        # expand towards +z direction
+        ref_xpnd = expanded3d[z_max] 
+        for z in range(z_max+1,num_z):
+            ref_wtsd = watershed3d[z]
+            chosen = find_hidden_object(ref_xpnd,ref_wtsd,centroid)
+            if chosen is None:
+                break
+            expanded3d[z,ref_wtsd==chosen] = prop.label
+            ref_xpnd = expanded3d[z]
+    return expanded3d
+# END of vacuole postprocesser
+
+
+def neighbor_mean(img_orga,img_cell):
+    """
+    fill the regions with cells with the mean of neighbouring background.
+    """
+    properties = measure.regionprops(img_cell)
+    img_out = np.copy(img_orga)
+    for prop in properties:
+        min_row, min_col, max_row, max_col = prop.bbox
+        win_row,win_col = max_row - min_row, max_col - min_col
+        min_row = max(min_row - win_row, 0)
+        min_col = max(min_col - win_col, 0)
+        max_row = min(max_row + win_row, img_cell.shape[0])
+        max_col = min(max_col + win_col, img_cell.shape[1])
+
+        bbox_bin = img_cell[min_row:max_row,min_col:max_col]
+        for z in range(img_orga.shape[0]):
+            bbox_orga = img_orga[z,min_row:max_row,min_col:max_col]
+            samples = bbox_orga[bbox_bin==0]
+            background = int(samples.mean())
+            idx = np.array([[z,*coo] for coo in prop.coords])
+            img_out[tuple(idx.T)] = background
+    return img_out
+
+def batch_apply(func,args:pd.DataFrame):
+    results = []
+    errmsgs  = []
+    for entry in args.iterrows():
+        try:
+            func(**entry[1])
+            results.append(True)
+            errmsgs.append("")
+        except Exception as e:
+            results.append(False)
+            errmsgs.append(repr(e))
+    args["RESULT"] = pd.Series(results)
+    args["ERR_MESSAGES"] = pd.Series(errmsgs)
+    return None
+
---
+++ organelle_measure/vars_allround1data.py
@@ -0,0 +1,373 @@
+
+
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+from os.path import expanduser
+
+def choose_directory():
+        print("Hello1")
+        input_dir = QFileDialog.getExistingDirectory(None, 'Select a folder:', expanduser("~"))
+     
+        return input_dir
+
+
+
+list_folders = [
+    "July25"
+]
+
+
+current_folder=list_folders[0]
+
+
+
+####new strain
+
+# path_measure="../../../Experiments/2024/new_strain"
+# path_in="../../../Experiments/2024/new_strain/"
+# path_out="../../../Analysis/2024/new_strain"
+
+
+
+
+
+
+
+# # ####noise_tradeoff
+
+path_measure="../../../Experiments/2024/noise_tradeoff"
+path_in="../../../Experiments/2024/noise_tradeoff/"
+path_out="../../../Analysis/2024/noise_tradeoff"
+
+
+
+
+####CU
+
+# path_measure="../../../Experiments/2024/CU"
+# path_in="../../../Experiments/2024/CU/"
+# path_out="../../../Analysis/2024/CU"
+
+
+
+####chxrapa
+
+# path_measure="../../../Experiments/2024/chxrapa"
+# path_in="../../../Experiments/2024/chxrapa/"
+# path_out="../../../Analysis/2024/chxrapa"
+
+
+
+
+
+
+out_fig1="../../../Analysis/2023_analysis/csv2_figure/nov"
+
+#for reading files
+out_chxrapa="../../../Analysis/2024/chxrapa"
+out_hu="../../../Analysis/2023_analysis/HU"
+out_chx="../../../Analysis/2023_analysis/CHX"
+out_bfa="../../../Analysis/2023_analysis/BFA"
+out_control="../../../Analysis/2023_analysis/control"
+chx_im="../../../Analysis/2023_analysis/CHX/chx_images"
+
+
+###==========xml file ie benchmark spectra
+path_xml="../../../Experiments/2023/xml_fileJan31"
+
+####chx
+
+# path_measure="../../../Experiments/2023/CHX"
+# path_in="../../../Experiments/2023/CHX/"
+# path_out="../../../Analysis/2023_analysis/CHX"
+
+### BFA
+# path_measure="../../../Experiments/2023/BFA"
+# path_in="../../../Experiments/2023/BFA/"
+# path_out="../../../Analysis/2023_analysis/BFA"
+
+
+####HU
+
+# path_measure="../../../Experiments/2023/HU"
+# path_in="../../../Experiments/2023/HU/"
+# path_out="../../../Analysis/2023_analysis/HU"
+
+
+
+
+
+
+##measurements
+
+subfolders_chxrapa = [
+    
+   "April03",
+  
+  "April04",
+  "April16",
+  
+    ]
+
+
+# path_measure="../../../Analysis/2023_analysis/CHX/chx_measurements"
+
+
+
+growth_rate_chx={
+
+    "0":"0.5",
+    "0.01":"0.4",
+    "0.1":"0.29",
+    "0.5":"0.16",
+    "2":"0.13",
+
+    }
+
+
+
+
+growth_rate_hu={
+
+    "0":"0.5",
+    "1.2":"0.44",
+    "4":"0.36",
+ 
+
+    }
+
+
+
+raw2sp=[
+    # "July22",
+    # "July25",
+    # "July28",
+    # "Aug01",
+    # "Aug02",
+    # "Aug05",
+    # "Aug06",
+    # "Aug07",
+# "Aug17",
+"Aug19",
+"Aug20",
+"Aug21",
+"Sept07",
+"sept29",
+"Sept30",
+"Oct02",
+"Oct03",
+# "Oct04",
+# "Oct05",
+"Oct07",
+]
+
+
+
+subfolders_chx = [
+    
+    "Aug01",
+    "Aug02",
+    "Aug07",
+    
+    #========================0.01================#
+    "Aug05",
+    "Aug19",
+    "Sept30",
+    "Oct07",
+    #========================0.1================#
+    "Aug06",
+    "Aug20",
+    "Oct02",
+    # "Oct04",
+    #========================0.5================#
+    # "Aug17",
+    "Aug21",
+    # "Oct05",
+    #========================2.0================#
+    "Sept07",
+    "Sept29",
+    "Oct03",
+    
+]
+subfolders_bfa = [
+    #========================25================#
+    
+    "Nov27","Nov28","Sept11",
+    #========================75================#
+    "Nov30","Sept12",
+    #========================100================#
+    
+    "Sept13",
+    
+]
+
+subfolders_control = [
+   
+    "Aug01",
+    "Aug02",
+    "Aug07",
+]
+
+
+subfolders_hu = [
+    #========================1.2================#
+    "Dec12",
+    "Nov06",
+    "Nov08",
+    #========================4================#
+    "Oct21"
+    
+    
+]
+
+
+organelles={
+    
+    
+    "peroxisome":"blue",
+    "vacuole":"blue",
+    "ER":"green",
+    "golgi":"yellow",
+    "mitochondria":"red",
+    "LD":"red",
+    "non-organelle":"non-organelle"
+    
+    
+    }
+
+
+
+output_folders={
+    "segmented_cell":"segmented_cell",
+    "organelle_unmixed":"organelle_unmixed",
+    "ilastk_segmentated":"ilastk_segmentated",
+    "label_organelle":"label_organelle",
+    "spectra":"spectra",
+    "measurements":"measurements",
+    "ilastk_control_libfile":"ilastk_control_libfile"
+
+    }
+
+
+
+experiments = {
+    "control":"control",
+    "cycloheximide": "Cycloheximide",
+    "bfa":     "bfa",
+    "hu":"hu"
+}
+
+growth_rate={
+    
+    "growth_chx":{"0.0":0.5,
+        "0.01":0.4,
+                  "0.1":0.29,
+                  "0.5":0.16,
+                  "2.0":0.13},
+    
+    
+    # "growth_chx":[0.4,0.29,0.16,0.13],
+    "growth_bfa":{
+    
+        "25.0":0.4,
+        "75.0":0.29,
+        "100.0":0.13
+        
+        },
+    "growth_hu":
+        
+        {
+            "1.2":0.4,
+                
+                "4":0.29
+            
+            
+            }
+        
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from statsmodels.stats.weightstats import ttest_ind
+# from statannotations.Annotator import Annotator
+# from scipy.stats import bootstrap
+# import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
+# from matplotlib import container
+# import scipy
+# from sklearn import metrics
+
+# ##get the number of cell in each condition
+# for cond in df_condition:
+#     df_bycell_volume=df_bycell.loc[ df_bycell["condition"].eq(cond)]
+#     print("condition:",cond,"independent measurement:",df_bycell_volume["folder"].unique())
+#     # print(df_bycell_volume1)
+#     print("Number of cells in each condition",cond,df_bycell_volume.shape[0]/7)
+
+'''
+
+#read the files
+df_bycell_control,df_orga_all = read_results(Path(out_control),subfolders_control,(px_x,px_y,px_z))
+df_bycell_chx,df_orga_all = read_results(Path(out_chx),subfolders_chx,(px_x,px_y,px_z))
+df_bycell_bfa,df_orga_all = read_results(Path(out_bfa),subfolders_bfa,(px_x,px_y,px_z))
+df_bycell=pd.concat([df_bycell_control,df_bycell_chx,df_bycell_bfa])
+
+# Apply the custom function to create the new column
+df_bycell['experiment'] = df_bycell.apply(custom_function, axis=1)
+
+'''
+
+
+    # cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    #  # Draw the heatmap with the mask and correct aspect ratio
+    # # More details at https://seaborn.pydata.org/generated/seaborn.heatmap.html
+    # sns.heatmap(
+    #     corrcoef,          # The data to plot
+    #     # mask=mask,     # Mask some cells
+    #     cmap=cmap,     # What colors to plot the heatmap as
+    #     annot=True,    # Should the values be plotted in the cells?
+    #     vmax=.3,       # The maximum value of the legend. All higher vals will be same color
+    #     vmin=-.3,      # The minimum value of the legend. All lower vals will be same color
+    #     center=0,      # The center value of the legend. With divergent cmap, where white is
+    #     square=True,   # Force cells to be square
+    #     linewidths=.5, # Width of lines that divide cells
+    #     cbar_kws={"shrink": .5}  # Extra kwargs for the legend; in this case, shrink by 50%
+    # )
+    
+    
+    
+
+# from termcolor import colored
+
+# # Sample variables
+# org = "Golgi"
+# var = "mean"
+# value = 42
+
+# # Define colors
+# org_color = 'red'
+# var_color = 'blue'
+# value_color = 'green'
+
+# # Print colored text with variable values
+# print(colored(f"Organelle: {org}", org_color))
+# print(colored(f"Variable: {var}", var_color))
+# print(colored(f"Value: {value}", value_color))
